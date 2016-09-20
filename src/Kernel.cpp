38ebdd341bc1d9e8994cd25a1f0312f796f5900e
David Schommer <djschomes@gmail.com>
2016-09-19 22:53:45 -0500

    add support for additional scene description variables

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -2,6 +2,8 @@
 #include <iostream> 
 #include <sstream> // string streams 
 
+#define MAX_COLOR_VALUE 255
+
 void Kernel::readScene(std::ifstream &inputFile) {
 	if (inputFile.is_open()) {
 		std::string line = ""; 
@@ -18,15 +20,43 @@ void Kernel::readScene(std::ifstream &inputFile) {
 			} 
 
 			else if (variable == "viewdir") {
-
+				double x = 0.0, y = 0.0, z = 0.0; 
+				ss >> x >> y >> z; 
+				viewingDirection = Vector3(x, y, z); 
 			}
 
 			else if (variable == "updir") {
-				
+				double x = 0.0, y = 0.0, z = 0.0; 
+				ss >> x >> y >> z; 
+				upDirection = Vector3(x, y, z); 
 			}
 
+			else if (variable == "fovv") {
+				double fovv = 0.0; 
+				ss >> fovv; 
+				verticalFieldOfView = fovv; 
+			}
 
+			else if (variable == "imsize") {
+				int width_in, height_in; 
+				ss >> width_in >> height_in; 
+				width = width_in; 
+				height = height_in; 
+			}
 
+			else if (variable == "bkgcolor") {
+				double r, g, b; 
+				ss >> r >> g >> b; 
+				// convert colors in range 0 - 1 to range 0 - 255
+				bkgcolor = RGB((int)(MAX_COLOR_VALUE * r), (int)(MAX_COLOR_VALUE * g), (int)(MAX_COLOR_VALUE * b)); 
+			}
+
+			else if (variable == "mtlcolor") {
+				double r, g, b; 
+				ss >> r >> g >> b; 
+				// convert colors in range 0 - 1 to range 0 - 255
+				materialColor = RGB((int)(MAX_COLOR_VALUE * r), (int)(MAX_COLOR_VALUE * g), (int)(MAX_COLOR_VALUE * b));
+			}
 
 
 		}
--- src/Kernel.hpp
+++ src/Kernel.hpp
@@ -3,12 +3,22 @@
 
 #include <fstream> 
 #include "Point3.hpp"
+#include "Vector3.hpp"
+#include "RGB.hpp"
 
 // The kernel is responsible for rendering routines and scene reading. 
 class Kernel {
 public: 
-
 	Point3 cameraPosition; 
+	Vector3 viewingDirection; 
+	Vector3 upDirection; 
+	double verticalFieldOfView; // in degrees 
+	int width; 
+	int height; 
+	RGB bkgcolor; 
+	RGB materialColor; 
+
+
 // PUBLIC
 	void readScene(std::ifstream &inputFile); 
 
