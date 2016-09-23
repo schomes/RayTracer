ca68af42fe110083779e5b41ffcae8b8a184c587
David Schommer <djschomes@gmail.com>
2016-09-23 06:31:15 -0500

    hit() function fixes

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -6,7 +6,7 @@
 
 double Sphere::hit(Ray &ray) {
 
-	std::cout << "entered" << std::endl; 
+
 	// for equation At^2 + Bt + c = 0
 
 	double x0 = (ray.origin).x; 
@@ -25,13 +25,18 @@ double Sphere::hit(Ray &ray) {
 	double B = 2 * (xd * (x0 - xc) + yd * (y0 - yc) + zd * (z0 - zc)); 
 	double C = pow((x0 - xc), 2.0) + pow((y0 - yc), 2.0) + pow((z0 - zc), 2.0) - pow(radius, 2.0); 
 
-	double discriminant = sqrt(pow(B, 2.0) - (4 * C)); 
+	std::cout << B << std::endl;
+	std::cout << C << std::endl;
+
+	double discriminant = pow(B, 2.0) - (4 * C); 
+
+	std::cout << "d: " << discriminant << std::endl;
 
 	if (discriminant < 0) {
 		return -1.0; 
 	} else {
-		double result1 = (-B + discriminant) / 2; 
-		double result2 = (-B - discriminant) / 2; 
+		double result1 = (-B + sqrt(discriminant)) / 2; 
+		double result2 = (-B - sqrt(discriminant)) / 2; 
 
 		double minValue = fmin(result1, result2); 
 		if (minValue < 0) { 
