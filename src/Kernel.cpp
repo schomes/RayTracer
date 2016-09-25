4971fa53aeae2e29d8f1a43c6bdae0fb178cd1ad
David Schommer <djschomes@gmail.com>
2016-09-25 16:51:27 -0500

    refactor Kernel

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -10,8 +10,8 @@
 #define MAX_COLOR_VALUE 255
 
 void Kernel::readScene(std::ifstream &inputFile) {
-	std::cout << "opened" << std::endl; 
 	if (inputFile.is_open()) {
+        std::cout << "opened and reading" << std::endl;
 		std::string line = ""; 
 		while(std::getline(inputFile, line)) {
 
@@ -89,12 +89,13 @@ Image Kernel::render() {
 	// Determine camera coordinate axes 
 	//... u is orthogonal to the viewingDirection
 	//... v is orthogonal to the viewingDirection and u
-	Vector3 u, v; 
+	Vector3 w, u, v;
 	//...TODO: check if viewingDirection and upDirection are close to parallel
 	//...TODO cont'd: more parallel means cross is closer to (0, 0, 0) (slide 6 - raycasting02.pdf)
-	u = viewingDirection.cross(upDirection); 
+    w = viewingDirection.normalize();
+	u = upDirection.cross(w);
 	u = u.normalize(); 
-	v = u.cross(viewingDirection); 
+	v = w.cross(u);
 	v = v.normalize(); 
 
 	// unit vector in the viewing direction 
@@ -115,19 +116,19 @@ Image Kernel::render() {
 	Point3 lr = cameraPosition + (d * n) - (viewHeight / 2 * v) + (viewWidth / 2 * u); 
 
 	// Horizontal offset per pixel
-	//Vector3 hOffset = (ur - ul) / (width - 1.0); 
+	Vector3 hOffset = (ur - ul) / (width - 1.0);
 	// Vertical offset per pixel 
-	//Vector3 vOffset = (ll - ul) / (height - 1.0); 
+	Vector3 vOffset = (ll - ul) / (height - 1.0);
 
 
 	for (int row = 0; row < height; row++) {
 		for (int column = 0; column < width; column++) {
-			//Point3 viewingWindowPoint = ul + (vOffset * row) + (hOffset * column); 
-			double xView = ul.x + (ur.x - ul.x) * (column + 0.5) / width; 
-			double yView = ll.y + (ul.y - ll.y) * (row + 0.5) / height; 
-			Point3 viewingWindowPoint = Point3(xView, yView, d); 
+			Point3 viewingWindowPoint = ul + (vOffset * row) + (hOffset * column);
+			/// double xView = ul.x + (ur.x - ul.x) * (column + 0.5) / width;
+			/// double yView = ll.y + (ul.y - ll.y) * (row + 0.5) / height;
+			/// Point3 viewingWindowPoint = Point3(xView, yView, -10);
 			//Vector3 rayDirection = viewingWindowPoint - cameraPosition; 
-			Vector3 rayDirection = (d * n) + (xView * u) + (yView * v);  
+			Vector3 rayDirection = viewingWindowPoint - cameraPosition;
 			rayDirection = rayDirection.normalize(); 
 			Ray ray = Ray(cameraPosition, rayDirection); 
 
@@ -147,18 +148,18 @@ RGB Kernel::TraceRay(Ray &ray) {
 	// For each object in scene, check for intersection (
 	// keep track of closest intersection, and that closest object) 
 	Sphere object; 
-	double minT = 0; 
+	double minT = 0;
 	// skip the first object since we already looked at it
 	for (int index = 0; index < objects.size(); index++) {
 		Sphere testObject = objects.at(index); 
-		double tempMinT = testObject.hit(ray); 
+		double tempMinT = testObject.hit(ray);
 
 		if ((tempMinT > 0) && ((minT == 0) || (tempMinT < minT))) {
 			object = testObject; 
 			minT = tempMinT; 
 		}
 	}
-
+    
 	// if 0, we didn't hit anything in front of us
 	if (minT == 0) {
 		return color; 
