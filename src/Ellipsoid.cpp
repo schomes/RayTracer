a745a51977c134620a83cda0535b528f831518ab
David Schommer <schom120@umn.edu>
2016-09-25 21:42:46 -0500

    add ellipsoid

---
+++ src/Ellipsoid.cpp
@@ -0,0 +1,8 @@
+#include "Ellipsoid.hpp"
+#include <cmath>
+
+#define NO_COLLISION_FLAG -1.0
+
+double Ellipsoid::hit(Ray &ray) {
+  return 0; 
+}
---
+++ src/Ellipsoid.hpp
@@ -0,0 +1,28 @@
+#ifndef ELLIPSOID
+#define ELLIPSOID
+
+#include "Surface.hpp"
+#include "Point3.hpp"
+#include "Material.hpp"
+#include "Ray.hpp"
+
+class Ellipsoid : public Surface {
+private:
+  double aRadius;
+  double bRadius;
+  double cRadius;
+
+public:
+  Ellipsoid() {}
+  Ellipsoid(Point3 center_in, double aRadius_in, double bRadius_in, double cRadius_in) {
+    center = center_in;
+    aRadius = aRadius_in;
+    bRadius = bRadius_in;
+    cRadius = cRadius_in;
+  }
+
+  double hit(Ray &ray);
+
+}; 
+
+#endif
