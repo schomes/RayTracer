155efd52a7d39ed943503018818990231dae6195
David Schommer <djschomes@gmail.com>
2016-09-25 19:03:28 -0500

    add Surface abstract class

--- src/Sphere.hpp
+++ src/Sphere.hpp
@@ -1,11 +1,12 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
+#include "Surface.hpp"
 #include "Point3.hpp"
 #include "Material.hpp"
 #include "Ray.hpp"
 
-class Sphere {
+class Sphere : public Surface {
 private: 
 	double radius; 
 	Point3 center;
@@ -14,9 +15,14 @@ private:
 public: 
 	Sphere() {} 
 	Sphere(Point3 center_in, double radius_in) : center(center_in), radius(radius_in) {}
+	
+	// Accessor methods 
 	const Point3 & getCenter() const {return center;}
 	Material getMaterial() {return material;}
+	
+	// Mutator methods
 	void setMaterial(Material &material_in) {material = material_in;}
+
 	double hit(Ray &ray);
 }; 
 
--- src/Surface.hpp
+++ src/Surface.hpp
@@ -5,24 +5,12 @@
 #include "Material.hpp"
 #include "Ray.hpp"
 
+/// This class represents a surface
 class Surface {
-private: 
-	Point3 _center;
-	Material _material; 
-	// prevent construction of base class
-	Surface(){}
-
-protected: 
-	Surface(Point3 center_in) : _center(center_in) {}
-	 
-
 public: 
-	const Point3 & getCenter() const {return _center;}
-	Material getMaterial() {return _material;}
-	void setMaterial(Material &material) {_material = material;}
+	Surface(){}
 	virtual double hit(Ray &ray) = 0;  
 	// virtual Box bounding-box(); 
-
 }; 
 
 #endif 
